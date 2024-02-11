1176c5e8c3cceeae9a7e6cb8ed9d87acdb9bf3f4
Miroslav Jovic <jovicmiroslav88@gmail.com>
2024-02-11 22:37:54 +0100

    small changes

--- js/Validator.js
+++ js/Validator.js
@@ -3,13 +3,13 @@ class NoviValidator {
 		this.elementsConfig = config;
 		this.formID = formID;
 		this.errors = {};
-		
+
 		this.generateErrorsObject();
 		this.inputListener();
 	}
 
 	generateErrorsObject() {
-		for(let field in this.elementsConfig) {
+		for (let field in this.elementsConfig) {
 			this.errors[field] = [];
 		}
 	}
@@ -17,7 +17,7 @@ class NoviValidator {
 	inputListener() {
 		let inputSelector = this.elementsConfig;
 
-		for(let field in inputSelector) {
+		for (let field in inputSelector) {
 			let el = document.querySelector(`${this.formID} input[name="${field}"]`);
 
 			el.addEventListener('input', this.validiraj.bind(this));
@@ -33,30 +33,30 @@ class NoviValidator {
 
 		this.errors[fieldName] = [];
 
-		if(elFields[fieldName].potrebno) {
-			if(fieldValue === '') {
-				this.errors[fieldName].push('Polje je prazno');
+		if (elFields[fieldName].potrebno) {
+			if (fieldValue === '') {
+				this.errors[fieldName].push('The field is empty');
 			}
 		}
 
-		if(elFields[fieldName].email) {
-			if(!this.validateEmail(fieldValue)) {
-				this.errors[fieldName].push('Neispravna email adresa');
+		if (elFields[fieldName].email) {
+			if (!this.validateEmail(fieldValue)) {
+				this.errors[fieldName].push('Invalid email address');
 			}
 		}
 
-		if(fieldValue.length < elFields[fieldName].minDuzina || fieldValue.length > elFields[fieldName].maxDuzina) {
-			this.errors[fieldName].push(`Polje mora imati minimalno ${elFields[fieldName].minDuzina} i maksimalno ${elFields[fieldName].maxDuzina} karaktera`);
+		if (fieldValue.length < elFields[fieldName].minDuzina || fieldValue.length > elFields[fieldName].maxDuzina) {
+			this.errors[fieldName].push(`The field must have a minimum ${elFields[fieldName].minDuzina} and maximum ${elFields[fieldName].maxDuzina} characters`);
 		}
 
-		if(elFields[fieldName].gadjaSeSa) {
+		if (elFields[fieldName].gadjaSeSa) {
 			let matchingEl = document.querySelector(`${this.formID} input[name="${elFields[fieldName].gadjaSeSa}"]`);
 
-			if(fieldValue !== matchingEl.value) {
-				this.errors[fieldName].push('Lozinke se ne poklapaju');
+			if (fieldValue !== matchingEl.value) {
+				this.errors[fieldName].push('Passwords do not match');
 			}
 
-			if(this.errors[fieldName].length === 0) {
+			if (this.errors[fieldName].length === 0) {
 				this.errors[fieldName] = [];
 				this.errors[elFields[fieldName].gadjaSeSa] = [];
 			}
@@ -66,8 +66,8 @@ class NoviValidator {
 	}
 
 	validationPassed() {
-		for(let key of Object.keys(this.errors)) {
-			if(this.errors[key].length > 0) {
+		for (let key of Object.keys(this.errors)) {
+			if (this.errors[key].length > 0) {
 				return false;
 			}
 		}
@@ -76,11 +76,11 @@ class NoviValidator {
 	}
 
 	populateErrors(errors) {
-		for(const elem of document.querySelectorAll('ul')) {
+		for (const elem of document.querySelectorAll('ul')) {
 			elem.remove();
 		}
 
-		for(let key of Object.keys(errors)) {
+		for (let key of Object.keys(errors)) {
 			let parentElement = document.querySelector(`${this.formID} input[name="${key}"]`).parentElement;
 			let errorsElement = document.createElement('ul');
 			parentElement.appendChild(errorsElement);
@@ -96,9 +96,9 @@ class NoviValidator {
 
 	validateEmail(email) {
 		if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
-	    	return true;
+			return true;
 		}
-	    
-	    return false;
+
+		return false;
 	}
 }
